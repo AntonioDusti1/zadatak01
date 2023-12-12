e5440b6aae2bb0a5f00fbce70a62bfad8f4d864d
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2023-12-12 12:24:29 +0100

    Add files via upload

---
+++ zadatak01.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a=100;
+		int b=3;
+		int x=a/b;
+		int y=a%b;
+		Console.WriteLine("a dijelimo sa b dobimo x i ostatak y",a,b,x,y);
+	}
+}
